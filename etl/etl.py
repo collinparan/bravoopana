2014a7ccd9517b058a6ea3a5fcda49d86d74e9af
Collin Paran <Paran_Philip@bah.com>
2023-03-12 23:29:30 -0600

    updated function

--- etl/etl.py
+++ etl/etl.py
@@ -83,44 +83,45 @@ def load_tsv(file_name):
     df.to_sql(file_name, con=engine, if_exists='replace', index=False)
     return f"Loaded {file_name}"
 
-# Set the directory path and CSV file name
-dir_path = '/path/to/directory'
-csv_file = 'file_info.csv'
+load_csv('test.csv')
+# # Set the directory path and CSV file name
+# dir_path = '/path/to/directory'
+# csv_file = 'file_info.csv'
 
-# Check if the CSV file exists, and create it if it doesn't
-if not os.path.exists(csv_file):
-    df = pd.DataFrame(columns=['filename', 'created_time', 'modified_time'])
-    df.to_csv(csv_file, index=False)
+# # Check if the CSV file exists, and create it if it doesn't
+# if not os.path.exists(csv_file):
+#     df = pd.DataFrame(columns=['filename', 'created_time', 'modified_time'])
+#     df.to_csv(csv_file, index=False)
 
-# Continuously check for new files
-while True:
-    # Loop through the files in the directory
-    for file_name in os.listdir(dir_path):
-        file_path = os.path.join(dir_path, file_name)
+# # Continuously check for new files
+# while True:
+#     # Loop through the files in the directory
+#     for file_name in os.listdir(dir_path):
+#         file_path = os.path.join(dir_path, file_name)
 
-        # Check if the file is a CSV
-        if not file_name.endswith('.csv'):
-            continue
+#         # Check if the file is a CSV
+#         if not file_name.endswith('.csv'):
+#             continue
 
-        # Get the file creation and modification times
-        created_time = os.path.getctime(file_path)
-        modified_time = os.path.getmtime(file_path)
+#         # Get the file creation and modification times
+#         created_time = os.path.getctime(file_path)
+#         modified_time = os.path.getmtime(file_path)
 
-        # Check if the file is new or has been modified
-        df = pd.read_csv(csv_file)
-        is_new_file = not any(df['filename'] == file_name)
-        is_modified_file = (df.loc[df['filename'] == file_name, 'modified_time'].values[0] != modified_time) if not is_new_file else False
+#         # Check if the file is new or has been modified
+#         df = pd.read_csv(csv_file)
+#         is_new_file = not any(df['filename'] == file_name)
+#         is_modified_file = (df.loc[df['filename'] == file_name, 'modified_time'].values[0] != modified_time) if not is_new_file else False
 
-        # If the file is new or has been modified, add/update the file info in the DataFrame and CSV
-        if is_new_file or is_modified_file:
-            df.loc[df['filename'] == file_name, ['created_time', 'modified_time']] = [created_time, modified_time] if not is_new_file else [created_time, modified_time, '']
-            if is_new_file:
-                df = df.append({'filename': file_name, 'created_time': created_time, 'modified_time': modified_time}, ignore_index=True)
-            df.to_csv(csv_file, index=False)
+#         # If the file is new or has been modified, add/update the file info in the DataFrame and CSV
+#         if is_new_file or is_modified_file:
+#             df.loc[df['filename'] == file_name, ['created_time', 'modified_time']] = [created_time, modified_time] if not is_new_file else [created_time, modified_time, '']
+#             if is_new_file:
+#                 df = df.append({'filename': file_name, 'created_time': created_time, 'modified_time': modified_time}, ignore_index=True)
+#             df.to_csv(csv_file, index=False)
 
-            # Optional: Print a message indicating the file was processed
-            print(f'Processed file: {file_name}')
+#             # Optional: Print a message indicating the file was processed
+#             print(f'Processed file: {file_name}')
 
-    # Wait for 60 seconds before checking again
-    time.sleep(60)
+#     # Wait for 60 seconds before checking again
+#     time.sleep(60)
     
